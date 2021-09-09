a07c0e70d765c356b32ab7340e22d15b28153ec2
Łukasz Klekner <lukaszklekner@gmail.com>
2021-09-09 18:51:04 +0200

    uses context and implements Transaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,11 +1,18 @@
-export const TransactionList = () => (
-  <>
-    <h3>History</h3>
-    <ul className='list'>
-      <li className='minus'>
-        Cash <span>-$400</span>
-        <button className='delete-btn'>x</button>
-      </li>
-    </ul>
-  </>
-)
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import { Transaction } from './Transaction'
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext)
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className='list'>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  )
+}

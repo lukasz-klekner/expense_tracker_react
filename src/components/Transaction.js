25e01784e11c8a3a8f94e36a57e234a6d661ba4c
Łukasz Klekner <lukaszklekner@gmail.com>
2021-09-10 07:55:38 +0200

    adds reducers and uses them

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,9 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+'
+  const { deleteTransaction } = useContext(GlobalContext)
 
   return (
     <>
@@ -8,7 +12,12 @@ export const Transaction = ({ transaction }) => {
         <span>
           {sign}${Math.abs(transaction.amount)}
         </span>
-        <button className='delete-btn'>x</button>
+        <button
+          onClick={() => deleteTransaction(transaction.id)}
+          className='delete-btn'
+        >
+          x
+        </button>
       </li>
     </>
   )
